353990b624315170a5ba7de84eaf81fd5b11d3e6
Lachlan McCarty <lachm@users.noreply.github.com>
2014-12-16 11:04:50 -0500

    Setup

---
+++ Conways.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class Conways {
+
+    private static JFrame frame = new JFrame("Conway's Game of Life");
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ ConwaysComponent.java
@@ -0,0 +1,5 @@
+import javax.swing.JComponent;
+
+public class ConwaysComponent extends JComponent {
+    s
+}
